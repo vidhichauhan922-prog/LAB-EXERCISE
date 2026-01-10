569b3cbd8b728ca85e18083a142a9cb7174972ca
VIDHI-CHAUHAN <vidhichauhan922@gmail.com>
2026-01-10 08:05:08 +0530

    Add files via upload

---
+++ LAB_TASK_ONE.py
@@ -0,0 +1,4 @@
+x="HELLO WORLD"
+print(x)
+print("python is a simple ,powerfull and widely used programming language.")
+print("Python is a versatile programming language used in web development ,data science ,and automation.")

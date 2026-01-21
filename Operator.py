933934436104fbf2bcef57b08fb684a44e9ad5d6
VIDHI-CHAUHAN <vidhichauhan922@gmail.com>
2026-01-21 16:31:54 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,68 @@
+# 1. Arithmetic Operators
+a = 200
+b = 20
+print("Arithmetic Operators:")
+print("a + b =", a + b)   # Addition
+print("a - b =", a - b)   # Subtraction
+print("a * b =", a * b)   # Multiplication
+print("a / b =", a / b)   # Division
+print("a % b =", a % b)   # Modulus
+print("a ** b =", a ** b) # Exponent
+print("a // b =", a // b) # Floor Division
+print()
+
+
+# 2. Assignment Operators
+x = 500
+print(x)
+
+
+#Unary minus 
+a=50
+print(-a)
+
+
+#Relational operator
+p=25
+q=5
+print(p>q)
+print(p==q)
+print(p<q)
+
+
+#Logical operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+
+#Boolean operator
+is_pass=True
+print(is_pass)
+
+
+#Bitwise operator
+a=5
+b=3
+print(a&b)
+print(a/b)
+
+
+#Membership operator
+number=[1,2,3,4]
+print(2 is number)
+print(5 not in number)
+
+
+#identity operator
+x=20
+y=10
+print(x is y)
+print(x is not y)
+
+
+
+
+

9d2fd68cde2397ed3f1b030751736bdee0906520
VIDHI-CHAUHAN <vidhichauhan922@gmail.com>
2026-01-21 15:25:59 +0530

    Add files via upload

---
+++ LAB-TASK-TWO.py
@@ -0,0 +1,89 @@
+print("hello world")
+
+num1 = int(input("enter the number: "))
+num2 = int(input("enter the number: "))
+sum = num1 + num2
+print("num1+num2:", sum)
+
+
+a=78
+b=35
+print(a+b)
+
+
+year = int(input("Enter a year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        print(year, "is a Leap Year")
+else:
+        print(year, "is not a Leap Year")
+        
+        
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+        print(num, "is an Even number")
+else:
+        print(num, "is an Odd number")
+        
+        
+import math
+print("PI value is:", math.pi)
+
+
+PI = 3.14  
+print("Constant value is:", PI)
+
+
+num = int(input("Enter a number: "))
+square = num * num
+print("Square of the number is:", square)
+
+
+import math
+radius = float(input("Enter radius of the circle: "))
+area = math.pi * radius * radius
+print("Area of the circle is:", area)
+
+
+a = 10
+b = 3.14
+c = "Hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+import math
+num = float(input("Enter a number: "))
+print("Square root:", math.sqrt(num))
+print("Power (num^2):", math.pow(num, 2))
+print("Absolute value:", math.fabs(num))
+print("Ceil value:", math.ceil(num))
+print("Floor value:", math.floor(num))
+print("Factorial (integer part):", math.factorial(int(num)))
+print("PI value:", math.pi)
+
+
+import math
+a = float(input("Enter base: "))
+b = float(input("Enter power: "))
+result = math.pow(a,b)
+print("power is:", result)
+
+
+num = float(input("Enter a number: "))
+if num > 0:
+        print(num, "is Positive")
+elif num < 0:
+        print(num, "is Negative")
+else:
+        print("The number is Zero")
+
+     
+
+
+
+        
+

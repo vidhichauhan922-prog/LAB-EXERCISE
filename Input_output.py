889da0e0963d9bfa321771c1fac845f596db09c0
VIDHI-CHAUHAN <vidhichauhan922@gmail.com>
2026-01-31 09:31:40 +0530

    Add files via upload

---
+++ Input_output.py
@@ -0,0 +1,24 @@
+print("HELLO PYTHON PROGRAMMING")
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+name = input("Enter your name: ")
+number=input("Enter your number")
+print("Hello", name,number)
+
+num=input("enter your number:")
+print("float number",num)
+
+first=input("enter first word:")
+second=input("enter second word:")
+print("you typed:",first,second)
+
+print("A","B","C")
+print("A","B","C",sep="-")
+
+name=input("enter student name")
+marks=input("enter your marks")
+print("student and marks:",name,marks)
+
+

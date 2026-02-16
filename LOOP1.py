a8cf61bbf557df2dfeafdffac7e218045c4e5aed
VIDHI-CHAUHAN <vidhichauhan922@gmail.com>
2026-02-16 15:16:33 +0530

    Add files via upload

---
+++ LOOP1.py
@@ -0,0 +1,62 @@
+#program:1
+
+num = int(input("enter any number : "))
+
+if num % 2 == 0:
+    print("this is even number.")
+else:
+    print("this is  odd number.")
+    
+ 
+#program:2  
+ 
+num = int(input("enter your number: "))
+
+if num > 0:
+    print("this number is positive .")
+else:
+    print("this number is negative .")
+
+
+#program:3
+
+num = int(input("enter your number: "))
+          
+if num == 0:
+    print("this number is zero.")
+else:
+    print("this number is not zero.")
+
+
+#program:4
+
+num = int(input("enter your number: "))
+
+if num > 10:
+    print("this number is bigger than 10.")
+else:
+    print("this number is bigger than 10 or equal.")
+
+
+#program:5
+
+marks = int(input("enter your marks: "))
+
+if marks >= 50:
+    print("you are pass.")
+else:
+    print("you are fail.")
+    
+    
+#program :6
+
+marks = float(input("ENTER YOUR MARKS: "))
+
+if marks >= 90:
+    print("your grade is A.")
+elif marks >= 75:
+    print("your grade is B.")
+elif marks >= 50:
+    print("your grade is C.")
+else:
+    print("your grade is D.")
